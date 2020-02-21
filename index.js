2a4a18696440c94388c333654d9b7acd4e2837c4
Emilio Sabatucci <sabatucci@gmail.com>
2020-02-21 08:39:46 +0100

    Move card node

---
+++ index.js
@@ -0,0 +1,69 @@
+module.exports = function(RED) {
+    "use strict";
+var http = require("follow-redirects").http;
+var https = require("follow-redirects").https;
+    
+function EmaMoveCard(n) {
+	RED.nodes.createNode(this,n);
+	
+	this.space = n.space;
+	this.name = n.name;
+	this.card = n.card;
+	this.username = n.username;
+	this.destination_list = n.destination_list;
+	
+	var node = this;
+	
+	node.on("input", function(msg) {
+	    if(msg.card){
+		node.card = msg.card;
+	    }
+	    if(msg.name){
+		node.name = msg.name;
+	    }
+	    if(msg.destination_list){
+		node.destination_list = msg.destination_list;
+	    }
+	    	    
+	    var data = { "destinationId":node.destination_list, "username": node.username };
+	    
+	    
+	    node.log("Move card:  " + node.username);
+	    
+	    node.send(msg);
+	    
+	    var post_data = JSON.stringify(data);
+	    
+	    var host = RED.settings.exentriq.boardsApiHost;
+	    
+	    var post_options = {
+	      "host": host,
+	      "path": "/api/entity"+msg.cardId+"/moveToList",
+	      "method": "POST",
+              "headers": {
+	        "Content-Type": "application/json"
+              }
+	    };
+	    
+	    node.status({fill:"green",shape:"dot",text:"moving card..."});
+	    var post_req = https.request(post_options, function (res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+			        node.status({});
+				msg.payload = chunk;
+				node.send(msg);
+			});
+	    }).on('error', function (err) {
+			node.status({fill:"red",shape:"dot",text:"move card error!"});
+			msg.payload = err;
+			node.send(msg);
+	    });
+		
+	    post_req.write(post_data);
+	    post_req.end();
+	});
+}
+    
+RED.nodes.registerType("move-card", EmaMoveCard);
+
+}
